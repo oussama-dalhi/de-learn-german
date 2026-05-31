67ac41d530d92cae3b1aae03ca2975432d4dab0c
oussama-dalhi <oussamadalhi00@gmail.com>
2026-05-31 19:50:00 +0100

    add more personality to setupscreen page

--- src/components/SetupScreen.jsx
+++ src/components/SetupScreen.jsx
@@ -8,21 +8,23 @@ function SetupScreen({
         <div className='setup-screen'>
             <div className="setup-card">
             <div className="titles">
-            <h1>German Learning App</h1>
-            <h2>Chose Your Level</h2>
+            <h1>🇩🇪 Learn German</h1>
+            <p className='description'>Master vocabulary with flashcards
+            and example sentences</p>
+            <h2>Choose your current level</h2>
             </div>
             <div className='levels'>
             <button className={selectedLevel === 'A1' ? 'active': ''} 
             onClick={() => setSelectedLevel('A1')} >
-                A1
+                A1 Beginner
             </button>
             <button className={selectedLevel === 'A2' ? 'active': ''} 
             onClick={() => setSelectedLevel('A2')}>
-                A2
+                A2 Elementary
             </button>
             <button className={selectedLevel === 'B1' ? 'active': ''} 
             onClick={() => setSelectedLevel('B1')}>
-                B1
+                B1 Intermediate
             </button>
             <button className="start-btn" 
             onClick={() => setAppStarted(true)}>
